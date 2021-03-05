cf8276e1db43b5c980620430cf975adf97ce9589
tazkyahumaira <79800861+tazkyahumaira@users.noreply.github.com>
2021-03-05 19:32:02 +0800

    Upload Website Portofolio
    Upload Website Portofolio

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const txtElement = ['Sistem Informasi'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik() {
+
+  if (count == txtElement.length) {
+    count = 0;
+  }
+
+  currentTxt = txtElement[count];
+
+  words = currentTxt.slice(0, ++txtIndex);
+  document.querySelector('.efek-ngetik').textContent = words;
+
+  setTimeout(ngetik, 500);
+
+})();
